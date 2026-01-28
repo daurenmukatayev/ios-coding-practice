a9fd5a512eed8d6372282ad0867e72f8b1dd7538
Dauren <daurewka.sdu@gmail.com>
2026-01-28 13:49:08 +0500

    feat: Add tab bar

---
+++ Bankey/Bankey/Utils/UIViewController+Utils.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewController.swift
+//  Bankey
+//
+//  Created by Educon IT on 28.01.2026.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground() // to hide Navigation Bar Line also
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+    func setTabBarImage(imageName: String, title: String){
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
