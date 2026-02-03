92f514e726cb808f0fd0e926505146f8e8a9aa4d
Dauren <daurewka.sdu@gmail.com>
2026-02-03 23:13:57 +0500

    test: Add unit tests for JSON parsing

---
+++ Bankey/BankeyUnitTests/AccountSummary/AccountTests.swift
@@ -0,0 +1,53 @@
+//
+//  AccountTests.swift
+//  BankeyUnitTests
+//
+//  Created by Educon IT on 03.02.2026.
+//
+
+import Foundation
+import XCTest
+
+@testable import Bankey
+
+class AccountTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testCanParse() {
+        let json = """
+                 [
+                   {
+                     "id": "1",
+                     "type": "Banking",
+                     "name": "Basic Savings",
+                     "amount": 929466.23,
+                     "createdDateTime" : "2010-06-21T15:29:32Z"
+                   },
+                   {
+                     "id": "2",
+                     "type": "Banking",
+                     "name": "No-Fee All-In Chequing",
+                     "amount": 17562.44,
+                     "createdDateTime" : "2011-06-21T15:29:32Z"
+                   },
+                  ]
+                """
+        let data = json.data(using: .utf8)!
+        let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+        
+        let result = try! decoder.decode([Account].self, from: data)
+        
+        XCTAssertEqual(result.count, 2)
+        
+        let account = result[0]
+        XCTAssertEqual(account.id, "1")
+        XCTAssertEqual(account.type, .Banking)
+        XCTAssertEqual(account.amount, 929466.23)
+        XCTAssertEqual(account.name, "Basic Savings")
+        XCTAssertEqual(account.createdDateTime.monthDayYearString, "Jun 21, 2010")
+    }
+}
--- Bankey/BankeyUnitTests/Utils/CurrencyFormatterTests.swift
+++ Bankey/BankeyUnitTests/Utils/CurrencyFormatterTests.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  CurrencyFormatterTests.swift
 //  BankeyUnitTests
 //
